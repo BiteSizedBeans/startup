cdb95eed2e0aad1e139a203926a56327e36cc49f
BiteSizedBeans <47thomasj@gmail.com>
2025-02-19 21:22:15 -0700

    fixed a bug

--- src/library/library.jsx
+++ src/library/library.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 export function Library() {
   return (
     <main className='main-lib'>
         <ul>
-            <li><a href="index.html">File1.mp3</a></li>
-            <li><a href="index.html">File2.mp3</a></li>
-            <li><a href="index.html">File3.mp3</a></li>
-            <li><a href="index.html">File4.mp3</a></li>
-            <li><a href="index.html">File5.mp3</a></li>
+            <li><NavLink className='nav-link' to='/'>File1.mp3</NavLink></li>
+            <li><NavLink className='nav-link' to='/'>File2.mp3</NavLink></li>
+            <li><NavLink className='nav-link' to='/'>File3.mp3</NavLink></li>
+            <li><NavLink className='nav-link' to='/'>File4.mp3</NavLink></li>
+            <li><NavLink className='nav-link' to='/'>File5.mp3</NavLink></li>
         </ul>
         <button type="submit">Upload New File</button>
     </main>
