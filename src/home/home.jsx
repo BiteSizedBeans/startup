7b04280b3a250e269a34f4d8d53a9584140e8a98
BiteSizedBeans <47thomasj@gmail.com>
2025-02-19 22:33:06 -0700

    play audion works now too!

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,21 +5,37 @@ export function Home() {
   const [response, setResponse] = useState('');
   const [question, setQuestion] = useState('');
   const [showTranscript, setShowTranscript] = useState(false);
+  const [playCurrentFile, setPlayCurrentFile] = useState(false);
+  const [currentFile, setCurrentFile] = useState('No File Selected');
 
   const toggleTranscript = () => {
     setShowTranscript(!showTranscript);
   }
 
+  const togglePlayCurrentFile = () => {
+    setPlayCurrentFile(!playCurrentFile);
+  }
+
   return (
     <main className='main'>
         <menu>
-          <button type="button" value="|Play Current File|">Play Current File</button>
+          <button type="button" value="|Play Current File|" 
+          onClick={(e) => {
+            e.preventDefault();
+            togglePlayCurrentFile();
+          }}>Play Current File</button>
           <button type="button" value="|View Transcript|" 
           onClick={(e) => {
             e.preventDefault();
             toggleTranscript();
           }}>View Transcript</button>
         </menu>
+        {playCurrentFile && (
+          <div>
+            <p>{currentFile}</p>
+            <audio src="../../../public/roll.mp3" controls />
+          </div>
+        )}
         {showTranscript && (
           <p>This is where the Transcript will go</p>
         )}
