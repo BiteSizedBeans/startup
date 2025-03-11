51c57cdd2ee9c9b08061109d0ff78facaf1cb9be
BiteSizedBeans <47thomasj@gmail.com>
2025-03-11 17:20:04 -0600

    quick fix

--- service/index.js
+++ service/index.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 require('dotenv').config();
 const OpenAI = require('openai');
 const { MongoClient } = require('mongodb');
-const config = require('dbConfig.json');
+const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
