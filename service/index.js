8420a29cf4b376021be421cf7e7b090cd7be6e40
BiteSizedBeans <47thomasj@gmail.com>
2025-03-11 22:57:52 -0600

    hardocded to use default file. I'll fix it in the future, but right now I need to debug some other things:
    Use the defaultUser whenever they're not logged in
    
    put the transcript on the main page

--- service/index.js
+++ service/index.js
@@ -89,10 +89,11 @@ apiRouter.post('/upload', upload.single('file'), async (req, res) => {
         const newPath = `${req.file.destination}${req.file.filename}.${extension}`;
         fs.renameSync(req.file.path, newPath);
         console.log(newPath);
-        const stream = fs.createReadStream(newPath);
-        const transcript = await openai.audio.transcriptions.create({
+        const transcript = await openai.audio.translations.create({
+            file: fs.createReadStream("uploads/Default_File.MP3"),
+            // file: fs.createReadStream("uploads/Test-1.MP3"),
             // file: fs.createReadStream("../public/Test (1).m4a"),
-            file: stream,
+            // file: fs.createReadStream(newPath);,
             model: "whisper-1"
         });
         console.log(transcript);
