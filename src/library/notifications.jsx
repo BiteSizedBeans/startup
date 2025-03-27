96e63a15bcf42083ac84145dd71e1d01f40dd0a4
BiteSizedBeans <47thomasj@gmail.com>
2025-03-27 08:08:48 -0600

    created notifications doc

---
+++ src/library/notifications.jsx
@@ -0,0 +1,11 @@
+export function Notifications({token}) {
+    return (
+        <div>
+            <ul>
+                <li>Notification 1</li>
+                <li>Notification 2</li>
+                <li>Notification 3</li>
+            </ul>
+        </div>
+    )
+}
